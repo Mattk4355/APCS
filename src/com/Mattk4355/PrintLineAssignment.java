5843d6c439bf76068d96f6cf290a007883acab68
Matthew Krawvzyk <mattk5678@gmail.com>
2016-09-09 13:11:36 -0700

    APCS Assignment 3 fixed

--- src/com/Mattk4355/PrintLineAssignment.java
+++ src/com/Mattk4355/PrintLineAssignment.java
@@ -6,7 +6,7 @@ package com.Mattk4355;
 public class PrintLineAssignment {
     public static void main(String[] args) {
 
-        System.out.println("Page 41 #13:"); //p48 #13
+        System.out.println("Page 48 #13:");
         System.out.println("\"Several slashes are sometimes seen, \"");
         System.out.println("said Sally. \"I\'ve said so.\" See?");
         System.out.println("\\ / \\\\ // \\\\\\ ///");
@@ -16,13 +16,13 @@ public class PrintLineAssignment {
         System.out.println("in \'string literals.\'");
         System.out.println("");
 
-        System.out.println("Page 41 #14:"); //p48 #14
+        System.out.println("Page 48 #14:");
         System.out.println("You\'re bound to \"get it right\"");
         System.out.println("if you read the section on");
         System.out.println("\' \'quotes.\' \'");
         System.out.println("");
 
-        System.out.println("Page 41 #15:"); //p48 #15
+        System.out.println("Page 49 #15:");
         System.out.println("/ \\ // \\\\ /// \\\\\\");
     }
 }
