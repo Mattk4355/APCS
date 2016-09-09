1926174189c270432f0c91a91d3aa278e2565220
Matthew Krawvzyk <mattk5678@gmail.com>
2016-09-09 13:01:40 -0700

    APCS Assignment 2

---
+++ src/com/Mattk4355/RocketShip.java
@@ -0,0 +1,63 @@
+// Matthew Krawczyk
+// Period 4 APCS
+// 09/07/16
+package com.Mattk4355;
+
+public class RocketShip {
+    public static void main(String[] args){
+        System.out.println("I am an awesome coder.");
+        System.out.print("This line doesn't end ");
+        System.out.print("...told you so. \n");
+        System.out.println("hi");
+
+        System.out.println("Rocket 1: ");
+        Rocket1();
+
+        System.out.println("Rocket 2: ");
+        Rocket2();
+
+    }
+    public static void Rocket1(){
+        System.out.print("        /\\ \n");
+        System.out.print("       /  \\ \n");
+        System.out.print("      /    \\ \n");
+        System.out.print("     /      \\ \n");
+        System.out.print("    /        \\ \n");
+        System.out.print("   /          \\ \n");
+        System.out.print("  /            \\ \n");
+        System.out.print("  |            | \n");
+        System.out.print("  |------------| \n");
+        System.out.print("  |  \"Chicken\" | \n");
+        System.out.print("  |------------| \n");
+        System.out.print("  |            | \n");
+        System.out.print("  |____________| \n");
+        System.out.print("     /\\    /\\ \n");
+        System.out.print("    /__\\  /__\\ \n");
+        System.out.print("     ||    || \n");
+        System.out.print("     ||    || \n");
+        System.out.print("     ||    || \n");
+        System.out.print("     ||    || \n");
+    }
+
+    public static void Rocket2(){
+        System.out.print("\t    /\\ \n");
+        System.out.print("\t   /  \\ \n");
+        System.out.print("\t  /    \\ \n");
+        System.out.print("\t /      \\ \n");
+        System.out.print("\t/        \\ \n");
+        System.out.print("   /  \t \t  \\ \n");
+        System.out.print("  / \t \t   \\ \n");
+        System.out.print("  |\t \t       | \n");
+        System.out.print("  |------------| \n");
+        System.out.print("  |  \"Chicken\" | \n");
+        System.out.print("  |------------| \n");
+        System.out.print("  |\t \t       | \n");
+        System.out.print("  |____________| \n");
+        System.out.print("     /\\    /\\ \n");
+        System.out.print("    /__\\  /__\\ \n");
+        System.out.print("     ||    || \n");
+        System.out.print("     ||    || \n");
+        System.out.print("     ||    || \n");
+        System.out.print("     ||    || \n");
+    }
+}
