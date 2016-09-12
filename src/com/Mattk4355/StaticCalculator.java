39a6f27e171b39b5dbea170ff456a4b16982fca5
Matthew Krawczyk <mattk5678@gmail.com>
2016-09-12 12:13:25 -0700

    Static Calculator

---
+++ src/com/Mattk4355/StaticCalculator.java
@@ -0,0 +1,12 @@
+package com.Mattk4355;
+
+public class StaticCalculator {
+    public static void main(String[] args) {
+        int a = 7, b = 3;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b) + " Remainder: " + (a % b));
+    }
+}
